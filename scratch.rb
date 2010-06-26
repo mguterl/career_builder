1ec2a09efde4843ad86a28a57c16ae9709d77eb5
Michael Guterl <mguterl@gmail.com>
2010-06-26 17:40:49 -0400

    use military field in example

--- scratch.rb
+++ scratch.rb
@@ -12,6 +12,4 @@ end
 
 client = CareerBuilder::Client.new("ryan@recruitmilitary.com", "AZG24N4S")
 
-# client.advanced_resume_search(:keywords => "ruby", :zip_code =>
-# "45140", :search_radius_in_miles =>5)
-puts client.get_resume(:resume_id => "RH52G867678F7GH02Q3")
+client.advanced_resume_search(:military_experience => "M1,M2,M3,M4,M5,M6,M7", :zip_code => "45140", :search_radius_in_miles => 150)
